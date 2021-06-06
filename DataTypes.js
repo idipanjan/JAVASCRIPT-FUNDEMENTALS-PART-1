091a9c462b0edc558a4ddac8c7adc89273eafc18
Dipanjan19 <53527762+Dipanjan19@users.noreply.github.com>
2021-06-06 21:08:08 +0530

    Create DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,52 @@
+//objects and data types
+/*
+premitive data type
+
+1.Numbers  //floating point numbers or normal integers (int+double all)
+2.String   //String
+3.Boolean  //true or flase
+
+4.Undefined   // let children
+5.Null  //empty value
+6.BigInt
+7.Symbol
+*/
+
+true;
+console.log(true);
+let isjavascript=true;
+console.log(isjavascript);
+console.log("\n");
+console.log(typeof(isjavascript));
+console.log(typeof(true));
+console.log("\n");
+console.log(typeof 'Daemon');
+console.log(typeof "Daemon");
+
+console.log(typeof 12);
+
+console.log("\n");
+
+//we can change the data type and value sof variable if we use 
+// let keyword in declaration of variavle
+console.log(typeof(isjavascript));   //boolean
+isjavascript='!yess';            
+console.log(typeof(isjavascript));  //string
+
+//undefined
+let date;
+console.log(date);
+console.log(typeof(date));  //uundefined
+
+//number
+var year=2021;
+console.log(typeof(year));  //number
+
+var year='2021';
+console.log(typeof(year));  //string
+
+//null
+console.log(typeof null);   //object
+
+
+
