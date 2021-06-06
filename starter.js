74fbd7f356619f39110fab80f61dd9edebad2618
Dipanjan19 <53527762+Dipanjan19@users.noreply.github.com>
2021-06-06 20:28:45 +0530

    Update starter.js

--- starter.js
+++ starter.js
@@ -14,15 +14,17 @@ console.log(person["dept"]);
 */
 
 
-var name="dipanjan";
-var age=21;
-var s=name.substring(1,4);
+var name="dipanjan";   //string
+var age=21;      //number
+var s=name.substring(1,4);  //string
 console.log(`name is: ${name}`);
 console.log(`subname is: ${s}`);
 console.log(`my age: ${age}`);
-var str=name.split("");
-console.log(str);
-console.log(typeof(str));
-console.log(typeof(name));
-console.log(name[0]);
+
+var str=name.split("");  //object
+console.log(str);      //object
+console.log(typeof(str));  //object
+console.log(typeof(name));  //string
+
+console.log(name[0]);    
 console.log(str[0]);
