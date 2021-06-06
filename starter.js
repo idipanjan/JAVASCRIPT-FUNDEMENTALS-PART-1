a9862d816b25444bfd4f3b0d4847280b95b3d5ec
Dipanjan19 <53527762+Dipanjan19@users.noreply.github.com>
2021-06-06 20:24:12 +0530

    Create starter.js

---
+++ starter.js
@@ -0,0 +1,28 @@
+//difference between Array and object in js
+/*
+var a=["Dip", "FSE", 1999];
+console.log(a[0]);
+
+var person={
+            Name:"Erix",
+            id: 107,
+            dept:"QEA"
+            };
+console.log(person.Name);
+console.log(person["dept"]);
+
+*/
+
+
+var name="dipanjan";
+var age=21;
+var s=name.substring(1,4);
+console.log(`name is: ${name}`);
+console.log(`subname is: ${s}`);
+console.log(`my age: ${age}`);
+var str=name.split("");
+console.log(str);
+console.log(typeof(str));
+console.log(typeof(name));
+console.log(name[0]);
+console.log(str[0]);
